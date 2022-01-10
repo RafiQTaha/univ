ddb6b08c828abf9566c4a5db32fc48b3e05b8a8a
Amine Moumni <childeroe12@gmail.com>
2022-01-10 10:35:19 +0100

    admission

--- src/Controller/GestionPreinscriptionController.php
+++ src/Controller/GestionPreinscriptionController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller\Preinscription;
+namespace App\Controller;
 
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/preinscription')]
-class GestionPreinscriptionController extends AbstractController
+class PreinscriptionController extends AbstractController
 {
     private $em;
     public function __construct(ManagerRegistry $doctrine)
