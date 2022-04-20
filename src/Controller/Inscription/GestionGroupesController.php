4f79e905b2d6929c15a0a4cf9fd3583648169bc6
RafiQ Taha <taharafik89@gmail.com>
2022-04-20 17:28:26 +0000

    'groupe'

---
+++ src/Controller/Inscription/GestionGroupesController.php
@@ -0,0 +1,233 @@
+<?php
+
+namespace App\Controller\Inscription;
+
+use App\Controller\ApiController;
+use App\Controller\DatatablesController;
+use App\Entity\AcEtablissement;
+use App\Entity\PGroupe;
+use App\Entity\TInscription;
+use Doctrine\Persistence\ManagerRegistry;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Reader\Xlsx as Reader;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+#[Route('/inscription/groupes')]
+class GestionGroupesController extends AbstractController
+{
+    private $em;
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->em = $doctrine->getManager();
+    }
+    #[Route('/', name: 'gestion_groupes')]
+    public function index(Request $request): Response
+    {
+         //check if user has access to this page
+        $operations = ApiController::check($this->getUser(), 'gestion_groupes', $this->em, $request);
+        if(!$operations) {
+            return $this->render("errors/403.html.twig");
+        }
+        return $this->render('inscription/gestion_groupes.html.twig', [
+            'etablissements' => $this->em->getRepository(AcEtablissement::class)->findAll(),
+            'operations' => $operations
+        ]);
+    }
+    
+    #[Route('/list', name: 'gestion_groupes_list')]
+    public function gestionInscriptionList(Request $request): Response
+    {
+        $params = $request->query;
+        // dd($params);
+        $where = $totalRows = $sqlRequest = "";
+        $filtre = "where 1 = 1";   
+        // dd($params->get('columns')[0]);
+        
+        if (!empty($params->get('columns')[0]['search']['value'])) {
+            // dd("in");
+            $filtre .= " and etab.id = '" . $params->get('columns')[0]['search']['value'] . "' ";
+        } 
+        if (!empty($params->get('columns')[1]['search']['value'])) {
+                $filtre .= " and form.id = '" . $params->get('columns')[1]['search']['value'] . "' ";
+        }    
+        if (!empty($params->get('columns')[2]['search']['value'])) {
+            $filtre .= " and prom.id = '" . $params->get('columns')[2]['search']['value'] . "' ";
+        }    
+        if (!empty($params->get('columns')[3]['search']['value'])) {
+            $filtre .= " and an.id = '" . $params->get('columns')[3]['search']['value'] . "' ";
+        }    
+        $columns = array(
+            array( 'db' => 'ins.id','dt' => 0),
+            array( 'db' => 'ins.code','dt' => 1),
+            array( 'db' => 'etu.nom','dt' => 2),
+            array( 'db' => 'etu.prenom','dt' => 3),
+            array( 'db' => 'etu.cne','dt' => 4),
+            array( 'db' => 'etu.cin','dt' => 5),
+            array( 'db' => 'etab.abreviation','dt' => 6),
+            array( 'db' => 'UPPER(form.abreviation)','dt' => 7),
+            array( 'db' => 'UPPER(prom.designation)','dt' => 8),
+            array( 'db' => 'LOWER(an.designation)','dt' => 9),
+            array( 'db' => 'st.designation','dt' => 10),
+            array( 'db' => 'grp.niveau','dt' => 11),
+           
+            
+        );
+        $sql = "SELECT " . implode(", ", DatatablesController::Pluck($columns, 'db')) . "
+        
+        FROM tinscription ins
+        inner join tadmission ad on ad.id = ins.admission_id
+        inner join tpreinscription pre on pre.id = ad.preinscription_id
+        inner join tetudiant etu on etu.id = pre.etudiant_id
+        inner join ac_annee an on an.id = ins.annee_id
+        inner join ac_formation form on form.id = an.formation_id              
+        inner join ac_etablissement etab on etab.id = form.etablissement_id 
+        INNER JOIN pstatut st ON st.id = ins.statut_id
+        inner join ac_promotion prom on prom.id = ins.promotion_id
+        left join pgroupe grp on grp.id = ins.groupe_id
+        
+        $filtre ";
+        // dd($sql);
+        $totalRows .= $sql;
+        $sqlRequest .= $sql;
+        $stmt = $this->em->getConnection()->prepare($sql);
+        $newstmt = $stmt->executeQuery();
+        $totalRecords = count($newstmt->fetchAll());
+        // dd($sql);
+        $my_columns = DatatablesController::Pluck($columns, 'db');
+            
+        // search 
+        $where = DatatablesController::Search($request, $columns);
+        if (isset($where) && $where != '') {
+            $sqlRequest .= $where;
+        }
+        $changed_column = $params->get('order')[0]['column'] > 0 ? $params->get('order')[0]['column'] - 1 : 0;
+        $sqlRequest .= " ORDER BY " .DatatablesController::Pluck($columns, 'db')[$changed_column] . "   " . $params->get('order')[0]['dir'] . "  LIMIT " . $params->get('start') . " ," . $params->get('length') . " ";
+        // $sqlRequest .= DatatablesController::Order($request, $columns);
+        
+        $stmt = $this->em->getConnection()->prepare($sqlRequest);
+        $resultSet = $stmt->executeQuery();
+        $result = $resultSet->fetchAll();
+        
+        
+        $data = array();
+        // dd($result);
+        $i = 1;
+        foreach ($result as $key => $row) {
+            $nestedData = array();
+            $cd = $row['id'];
+            $nestedData[] = "<input type ='checkbox' class='check_admissible' id ='$cd' >";
+            $nestedData[] = $cd;
+            // dd($row);
+            
+            foreach (array_values($row) as $key => $value) {
+                if($key > 0) {
+                    if ($key == 11) {
+                        $nestedData[] = "<center><b>$value</b></center>";
+                    }else {
+                        $nestedData[] = $value;
+                    }
+                }
+            }
+            $nestedData["DT_RowId"] = $cd;
+            $nestedData["DT_RowClass"] = $cd;
+            $data[] = $nestedData;
+            $i++;
+        }
+        // dd($data);
+        $json_data = array(
+            "draw" => intval($params->get('draw')),
+            "recordsTotal" => intval($totalRecords),
+            "recordsFiltered" => intval($totalRecords),
+            "data" => $data   
+        );
+        // die;
+        return new Response(json_encode($json_data));
+    }
+
+    #[Route('/affectation_canvas', name: 'affectation_canvas')]
+    public function affectation_canvas(): Response
+    {   
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+        $sheet->setCellValue('A1', 'Inscription_id');
+        $sheet->setCellValue('B1', 'Groupe_id');
+        $writer = new Xlsx($spreadsheet);
+        $fileName = 'affectation_canvas.xlsx';
+        $temp_file = tempnam(sys_get_temp_dir(), $fileName);
+        $writer->save($temp_file);
+        return $this->file($temp_file, $fileName, ResponseHeaderBag::DISPOSITION_INLINE);
+    }
+
+    #[Route('/groupes_canvas', name: 'groupes_canvas')]
+    public function groupes_canvas(): Response
+    {   
+        $groupes = $this->em->getRepository(PGroupe::class)->findAll();
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+        $sheet->setCellValue('A1', 'Id');
+        $sheet->setCellValue('B1', 'Niveau');
+        $i=2;
+        foreach ($groupes as $groupe) {
+            $sheet->setCellValue('A'.$i, $groupe->getId());
+            $sheet->setCellValue('B'.$i, $groupe->getNiveau());
+            $i++;
+        }
+        $writer = new Xlsx($spreadsheet);
+        $fileName = 'groupes_canvas.xlsx';
+        $temp_file = tempnam(sys_get_temp_dir(), $fileName);
+        $writer->save($temp_file);
+        return $this->file($temp_file, $fileName, ResponseHeaderBag::DISPOSITION_INLINE);
+    }
+    
+    #[Route('/import_groupe', name: 'import_groupe')]
+    public function import_groupe(Request $request, SluggerInterface $slugger): Response
+    {
+        $file = $request->files->get('file');
+        if(!$file){
+            return new JsonResponse('Prière d\'importer le fichier',500);
+        }
+        if($file->guessExtension() !== 'xlsx'){
+            return new JsonResponse('Prière d\'enregister un fichier xlsx', 500);            
+        }
+        $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+        // this is needed to safely include the file name as part of the URL
+        $safeFilename = $slugger->slug($originalFilename);
+        $newFilename = $safeFilename.'-'.uniqid().'_'.$this->getUser()->getUsername().'.'.$file->guessExtension();
+
+        // Move the file to the directory where brochures are stored
+        try {
+            $file->move(
+                $this->getParameter('inscription_groupes_directory'),
+                $newFilename
+            );
+        } catch (FileException $e) {
+            throw new \Exception($e);
+        }
+        $reader = new reader();
+        $spreadsheet = $reader->load($this->getParameter('inscription_groupes_directory').'/'.$newFilename);
+        $worksheet = $spreadsheet->getActiveSheet();
+        $spreadSheetArys = $worksheet->toArray();
+
+        unset($spreadSheetArys[0]);
+        $sheetCount = count($spreadSheetArys);
+        
+        foreach ($spreadSheetArys as $sheet) {
+            if($sheet[0] != Null){
+                $inscription = $this->em->getRepository(TInscription::class)->find($sheet[0]);
+                if ($inscription != Null) {
+                    $inscription->setGroupe($this->em->getRepository(PGroupe::class)->find($sheet[1]));
+                }
+            }
+        }
+        $this->em->flush();
+        return new Response('Inscription Bien Modifier!',200);
+    }
+}
