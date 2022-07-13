8b0ab02f9abe21e0cd682b9f4708ea9d17c35e9b
RafiQ Taha <taharafik89@gmail.com>
2022-07-13 18:23:54 +0100

    'double_fac'

--- src/Controller/Facture/GestionFactureController.php
+++ src/Controller/Facture/GestionFactureController.php
@@ -69,7 +69,7 @@ class GestionFactureController extends AbstractController
          
         $params = $request->query;
         $where = $totalRows = $sqlRequest = "";
-        $filtre = " where 1=1 ";
+        $filtre = " where 1=1 and stat.designation = 'INSCRIT' ";
         
         if (!empty($params->all('columns')[0]['search']['value'])) {
             $filtre .= " and etab.id = '" . $params->all('columns')[0]['search']['value'] . "' ";
