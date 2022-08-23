0694676ac51080569c75e050aa61194aa4ef04b5
RafiQ Taha <taharafik89@gmail.com>
2022-08-23 18:44:51 +0100

    'extra-ins'

--- src/Repository/TInscriptionRepository.php
+++ src/Repository/TInscriptionRepository.php
@@ -54,7 +54,7 @@ class TInscriptionRepository extends ServiceEntityRepository
             ->innerJoin("t.admission", "admission")
             ->innerJoin("admission.preinscription", "preinscription")
             ->innerJoin("preinscription.etudiant", "etudiant")
-            ->innerJoin("preinscription.nature", "nature")
+            ->leftJoin("preinscription.nature", "nature")
             // ->Where("annee.validation_academique = 'non'")
             // ->andWhere("annee.cloture_academique = 'non'")
             ->Where("t.statut = 13")
