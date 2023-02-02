cbbfe194b9eeb28be46f8156f7c4e08c98ddaabe
RafiQ Taha <taharafik89@gmail.com>
2023-02-02 11:43:41 +0100

    'mise_en_page'

--- src/Controller/Evaluation/ElementController.php
+++ src/Controller/Evaluation/ElementController.php
@@ -169,7 +169,7 @@ class ElementController extends AbstractController
             'margin_left' => '5',
             'margin_right' => '5',
             'margin_top' => '35',
-            'margin_bottom' => '35',
+            'margin_bottom' => '20',
             'format' => 'A4-L',
             'margin_header' => '2',
             'margin_footer' => '2'
